0188143424986637852d119ef42499c065d55ae3
Abhi <ranjanabhi2468@gmail.com>
2025-02-15 02:15:16 +0530

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,20 @@
 "use client";
-import { Geist_Mono as GeistMono, Geist as GeistSans } from "next/font/google";
-
+import { Inter, Roboto_Mono } from "next/font/google";
 
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 import { FlaskResponseProvider } from "./context/FlaskResponseContext";
-const geistSans = GeistSans({
+
+const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-geist-sans",
+  variable: "--font-inter",
 });
 
-const geistMono = GeistMono({
+const robotoMono = Roboto_Mono({
   subsets: ["latin"],
-  variable: "--font-geist-mono",
+  variable: "--font-roboto-mono",
 });
-
 
 export default function RootLayout({
   children,
@@ -25,34 +24,32 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <FlaskResponseProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <header className="flex justify-between">
-          <SignedOut>
-          <div className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-            <SignInButton />
-          </div>
-            </SignedOut>
-            <SignedIn>
-            <div className="flex items-center space-x-2">
-               <UserButton
-               showName
-               appearance={{
-                elements: {
-                  userButtonBox: "bg-gray-900 text-white hover:bg-gray-700 rounded-full p-2",
-                  userButtonText: "text-white",
-                },
-              }}
-              />
-              </div>
-            </SignedIn>
-        </header>
-        {children}
-      </body>
-    </html>
-    </FlaskResponseProvider>
+        <html lang="en">
+          <body className={`${inter.variable} ${robotoMono.variable} antialiased`}>
+            <header className="flex justify-between">
+              <SignedOut>
+                <div className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                  <SignInButton />
+                </div>
+              </SignedOut>
+              <SignedIn>
+                <div className="flex items-center space-x-2">
+                  <UserButton
+                    showName
+                    appearance={{
+                      elements: {
+                        userButtonBox: "bg-gray-900 text-white hover:bg-gray-700 rounded-full p-2",
+                        userButtonText: "text-white",
+                      },
+                    }}
+                  />
+                </div>
+              </SignedIn>
+            </header>
+            {children}
+          </body>
+        </html>
+      </FlaskResponseProvider>
     </ClerkProvider>
   );
 }
